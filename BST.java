e0e360db54c739bed709ad8c28a8ff4571597871
patilastik <patilastik7@gmail.com>
2026-06-11 16:25:26 +0530

    Created a class BST i.e binary search tree with one more class Node

---
+++ BST.java
@@ -0,0 +1,21 @@
+// Start date:- 11/06/26
+
+class Node{
+
+    Node left; // by default it contains null
+    int item; // by default it stores 0
+    Node right; // by default it contains null
+
+    public Node(int data) // Constructor
+     {
+        item = data;
+     }
+}
+
+public class BST {
+
+    Node root;
+    public static void main(String[] args) {
+        
+    }
+}
